6447659a2847914cf442e5720c09b3c5ba520a4e
SAITO-KO87 <karei2640@gmail.com>
2025-05-03 20:00:03 +0900

    db機能追加

---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php
+define('DB_DSN', 'mysql:host=localhost;dbname=blog_db;charset=utf8');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
--- config/db.php
+++ config/db.php
@@ -1,12 +1,11 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=blog_db;charset=utf8';
-$user = 'root';
-$password = '';
+require_once __DIR__ . '/../config/config.php';
 
 try {
-    $pdo = new PDO($dsn, $user, $password);
+    $pdo = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     echo 'DB接続エラー: ' . $e->getMessage();
     exit;
 }
-?>
