c66d3c2f142422db0541f61b6fbcd63a71c3df68
SAITO-KO87 <karei2640@gmail.com>
2025-05-06 19:18:26 +0900

    indexページ編集

--- public/index.php
+++ public/index.php
@@ -10,15 +10,16 @@ $posts = $stmt->fetchAll();
 <ul>
 <?php foreach ($posts as $post): ?>
     <li>
-        <a href="show.php?id=<?= $post['id'] ?>"><?= htmlspecialchars($post['title']) ?></a>
+        <strong><a href="show.php?id=<?= $post['id'] ?>"><?= htmlspecialchars($post['title']) ?></a></strong><br>
+        <small>投稿日: <?= htmlspecialchars($post['created_at']) ?></small><br>
         <?php if (isset($_SESSION['user_id'])): ?>
-            [<a href="edit.php?id=<?= $post['id'] ?>">編集</a>]
-            [<a href="delete.php?id=<?= $post['id'] ?>" onclick="return confirm('削除しますか？');">削除</a>]
+            <a href="edit.php?id=<?= $post['id'] ?>"><button>編集</button></a>
+            <a href="delete.php?id=<?= $post['id'] ?>" onclick="return confirm('削除しますか？');"><button>削除</button></a>
         <?php endif; ?>
     </li>
 <?php endforeach; ?>
 </ul>
 
-<a href="new.php">新規投稿</a>
+<a href="new.php"><button>新規投稿</button></a>
 
 <?php include __DIR__ . '/../templates/footer.php'; ?>
