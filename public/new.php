3089f0750c32ef6084c66b7b9aa05d4f1968f666
SAITO-KO87 <karei2640@gmail.com>
2025-05-06 21:22:21 +0900

    新規投稿フォームのラベルを追加し、内容入力欄を必須に設定。戻るリンクをボタンに変更。

--- public/new.php
+++ public/new.php
@@ -6,12 +6,13 @@ include __DIR__ . '/../templates/header.php';
 
 <h2>新規投稿</h2>
 <form action="create.php" method="post">
-    タイトル: <input type="text" name="title"><br>
-    内容:<br>
-    <textarea name="content" rows="10" cols="50"></textarea><br>
+    <label>タイトル:</label><br>
+    <input type="text" name="title" required><br>
+    <label>内容:</label><br>
+    <textarea name="content" rows="10" required></textarea><br>
     <input type="submit" value="投稿">
 </form>
 
-<a href="index.php">← 戻る</a>
+<a href="index.php"><button>戻る</button></a>
 
 <?php include __DIR__ . '/../templates/footer.php'; ?>
